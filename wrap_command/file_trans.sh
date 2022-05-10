da9cd7be36770f1f737c22773e8ec00926db65c4
Mozart <31980988+MoZeWei@users.noreply.github.com>
2022-05-10 20:18:42 +0800

    Delete file_trans.sh

--- wrap_command/file_trans.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-read -p "File path(without name): " file_path
-read -p "File name: " file_name
-read -p "dst path(beyond /home/mzw/):" dst_path
-
-scp ${file_path}/${file_name} mzw@nscc-gz.wu-kan.cn:/home/mzw/${dst_path}
